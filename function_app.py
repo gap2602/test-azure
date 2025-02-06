b6e051692059a6c8e1e5349cfd8c4522e93317e0
gap2602 <gap@davoy.tech>
2025-02-06 10:56:02 +0700

    first init

---
+++ function_app.py
@@ -0,0 +1,28 @@
+import azure.functions as func
+import logging
+from helpers import helper_function
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="add_name")
+def add_name(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    helper_message = helper_function()
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. {helper_message}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
---
+++ helpers.py
@@ -0,0 +1,4 @@
+# helpers.py
+
+def helper_function():
+    return "This is a helper function!"
